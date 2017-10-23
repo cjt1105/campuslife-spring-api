237833eac9e97f1bbf5cc418cf2724bd42bc6ea5
CJT1105 <CJT1105@GMAIL.COM>
2017-10-23 01:18:42 -0500

    added lombok to all entities

--- src/main/java/com/cjtate/campuslifeapi/domain/Post.java
+++ src/main/java/com/cjtate/campuslifeapi/domain/Post.java
@@ -1,54 +1,27 @@
 package com.cjtate.campuslifeapi.domain;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.neo4j.ogm.annotation.GraphId;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
-import lombok.Getter;
-import lombok.Setter;
+
+import lombok.Data;
 
 @NodeEntity
-public class Post {
+@Data public class Post {
 	
 	@GraphId
 	private Long id;
 	
-	@Getter @Setter private String body;
+	private String body;
 	
-	@Getter @Setter private User postedBy;
+	private User postedBy;
 	
 	@Relationship(type="IS_COMMENT", direction=Relationship.INCOMING)
 	private List <Comment> comments = new ArrayList<>();
 	
 	@Relationship(type="LIKES_POST", direction=Relationship.INCOMING)
 	private List <User> likes = new ArrayList<>();
-	
-	public Post() {
-		
-	}
-	
-	public Post(String body) {
-		this.body = body;
-	}
-	
-	public void addComment(Comment comment) {
-		this.comments.add(comment);
-	}
-	
-	public Collection<Comment> getComments() {
-		return comments;
-	}
-	
-	public void addLike(User user) {
-		this.likes.add(user);
-	}
-	
-	public Collection<User> getLikes() {
-		return likes;
-	}
-	
-
 }
